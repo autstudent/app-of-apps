8c5dc00d5a88ef0e8f2d9cf597ecf827ea963150
Asier Cidon <asier.cidon@gmail.com>
2024-01-18 15:34:25 +0100

    Added some typos

--- scripts/onboarding.sh
+++ scripts/onboarding.sh
@@ -1,6 +1,11 @@
 oc create serviceaccount k8sadmin -n default
-oc adm policy add-cluster-role-to-user admin system:serviceaccount:default:k8sadmin
+
+oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:default:k8sadmin
+
+oc adm policy add-cluster-role-to-user admin system:serviceaccount:openshift-gitops:openshift-gitops-argocd-server
+
 oc -n default describe secret $(oc -n default get secret | (grep k8sadmin || echo "$_") | awk '{print $1}') | grep token: | awk '{print $2}'
 
 oc new-project authentication
+
 oc new-project monitoring
